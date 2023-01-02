02b34237e5d7d16045dd99c3289ff1958aee07a7
Abdelmasieh01 <gokuabdo31@gmail.com>
2023-01-03 01:34:31 +0200

    Trying whitenoise again

--- yosr/settings.py
+++ yosr/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_DIRS = [BASE_DIR / "staticfiles"]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
