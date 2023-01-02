75d5110bb2d84fd1afde84dfc7a041ab9e0e585b
Abdelmasieh01 <gokuabdo31@gmail.com>
2023-01-03 01:10:30 +0200

    port

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn yosr.wsgi --log file -
+web: gunicorn yosr.wsgi --log-file -
--- yosr/settings.py
+++ yosr/settings.py
@@ -83,7 +83,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        'PORT': config('PORT')
     }
 }
 
