4f0f02939d4f9cefdf88a182ff3a3b3ac7e277e5
Abdelmasieh01 <gokuabdo31@gmail.com>
2023-01-09 19:15:46 +0200

    Adding render host

--- yosr/settings.py
+++ yosr/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['yosr.up.railway.app', 'https://yosr.onrender.com/', 'localhost']
+ALLOWED_HOSTS = ['yosr.up.railway.app', 'yosr.onrender.com', 'localhost']
 
 
 # Application definition
